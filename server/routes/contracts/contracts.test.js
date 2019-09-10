5158de3ea04242bce63ae6a1d51ed7a46c49bdc1
Zach Sherman <zach@shrmn.toys>
2019-09-10 17:44:24 -0400

    fix contracts tests

--- server/routes/contracts/contracts.test.js
+++ server/routes/contracts/contracts.test.js
@@ -1,5 +1,6 @@
-const app = require('../../../server')
 const supertest = require('supertest')
+const app = require('../../../server')
+const { getSmartContracts } = require('../../state')
 const request = supertest(app)
 
 const demoSmartContract = {
@@ -13,10 +14,12 @@ test('POST well-formed smart contract', async () => {
     .post('/api/v0/contracts')
     .send(demoSmartContract)
     .expect(200)
+
+  expect(getSmartContracts()).toMatchObject([demoSmartContract])
 })
 
-test('POST malformed smart contract', async () => {
-  await request
+test('POST malformed smart contract', () => {
+  request
     .post('/api/v0/contracts')
     .send({ wrong: 'structure' })
     .expect(400)
