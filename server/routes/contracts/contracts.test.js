4a3cbb4a6e75c8bf39152290bb22dcac68aa2bc0
Zach Sherman <zach@shrmn.toys>
2019-09-10 18:11:58 -0400

    fix contracts tests

--- server/routes/contracts/contracts.test.js
+++ server/routes/contracts/contracts.test.js
@@ -1,5 +1,6 @@
-const app = require('../../../server')
 const supertest = require('supertest')
+const app = require('../../../server')
+const { getSmartContracts, initSmartContracts } = require('../../state')
 const request = supertest(app)
 
 const demoSmartContract = {
@@ -8,16 +9,25 @@ const demoSmartContract = {
   abi: 'whatever'
 }
 
-test('POST well-formed smart contract', async () => {
-  await request
+test('POST well-formed smart contract', () => {
+  request
     .post('/api/v0/contracts')
     .send(demoSmartContract)
     .expect(200)
 })
 
-test('POST malformed smart contract', async () => {
-  await request
+test('Save well-formed smart contract', async () => {
+  initSmartContracts()
+  await request.post('/api/v0/contracts').send(demoSmartContract)
+  expect(getSmartContracts()).toMatchObject([demoSmartContract])
+})
+
+test('POST malformed smart contract', () => {
+  request
     .post('/api/v0/contracts')
     .send({ wrong: 'structure' })
     .expect(400)
+    .expect(
+      'Error: smartContract is missing or invalid. Error: network is missing or invalid. Error: abi is missing or invalid.'
+    )
 })
