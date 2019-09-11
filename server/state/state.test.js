7e2e364851416b94b4ae34f0fbeaa88beee93cb4
radio-alice <42726587+radio-alice@users.noreply.github.com>
2019-09-11 10:09:24 -0400

    Add linting, Travis CI, and test clean enhancements (#4)
    * add lint, prettier configs
    
    * fix prettier/linting errors for all files
    
    * add husky pre-commit
    
    * Config/test coverage (#6)
    
    * add travis ci
    
    * fix merge conflicts
    
    * add node version
    
    * fix contracts tests
    
    * fix contracts tests
    
    * fix merge
    
    * fix async contract tests
    
    * fix async contract tests
    
    * working locally
    
    * change test command

--- server/state/state.test.js
+++ server/state/state.test.js
@@ -1,45 +1,45 @@
 const {
-	addSmartContract,
-	getSmartContracts,
-	initSmartContracts
+  addSmartContract,
+  getSmartContracts,
+  initSmartContracts
 } = require('./index')
 
 const demoSmartContract = {
-	smartContract: 'asdfasdfjahkj',
-	network: 'mainnet',
-	abi: 'whatever'
+  smartContract: 'asdfasdfjahkj',
+  network: 'mainnet',
+  abi: 'whatever'
 }
 
 test('add/get smart contract', () => {
-	initSmartContracts()
-	addSmartContract(demoSmartContract)
-	expect(getSmartContracts()).toMatchObject([demoSmartContract])
+  initSmartContracts()
+  addSmartContract(demoSmartContract)
+  expect(getSmartContracts()).toMatchObject([demoSmartContract])
 })
 
 test('add two smart contracts', () => {
-	initSmartContracts()
-	addSmartContract(demoSmartContract)
-	addSmartContract(demoSmartContract)
+  initSmartContracts()
+  addSmartContract(demoSmartContract)
+  addSmartContract(demoSmartContract)
 
-	expect(getSmartContracts().length).toBe(2)
+  expect(getSmartContracts().length).toBe(2)
 })
 
 test('robustly handle malformed smart contracts', () => {
-	expect(addSmartContract({ wrong: 'keys' })).toBe(
-		'Error: smartContract is missing or invalid. Error: network is missing or invalid. Error: abi is missing or invalid.'
-	)
+  expect(addSmartContract({ wrong: 'keys' })).toBe(
+    'Error: smartContract is missing or invalid. Error: network is missing or invalid. Error: abi is missing or invalid.'
+  )
 
-	expect(addSmartContract({ smartContract: 'keys' })).toBe(
-		'Error: network is missing or invalid. Error: abi is missing or invalid.'
-	)
+  expect(addSmartContract({ smartContract: 'keys' })).toBe(
+    'Error: network is missing or invalid. Error: abi is missing or invalid.'
+  )
 
-	expect(
-		addSmartContract({ smartContract: 'keys', network: 'wrongnetwork' })
-	).toBe(
-		'Error: network is missing or invalid. Error: abi is missing or invalid.'
-	)
+  expect(
+    addSmartContract({ smartContract: 'keys', network: 'wrongnetwork' })
+  ).toBe(
+    'Error: network is missing or invalid. Error: abi is missing or invalid.'
+  )
 
-	expect(addSmartContract({ smartContract: 'keys', network: 'rinkeby' })).toBe(
-		'Error: abi is missing or invalid.'
-	)
+  expect(addSmartContract({ smartContract: 'keys', network: 'rinkeby' })).toBe(
+    'Error: abi is missing or invalid.'
+  )
 })
