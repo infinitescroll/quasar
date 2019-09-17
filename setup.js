5bd6ad4540e517bf3302a94b69264be7202dc483
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-09-17 17:04:02 -0400

    hardcode ipfs api address

--- setup.js
+++ setup.js
@@ -4,5 +4,11 @@ const { compileAndMigrate, startGanache } = require('./scripts')
 module.exports = async () => {
   await startGanache()
   await compileAndMigrate()
-  await new IPFS()
+  new IPFS({
+    config: {
+      Addresses: {
+        API: `/ip4/127.0.0.1/tcp/5001`
+      }
+    }
+  })
 }
