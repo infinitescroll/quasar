68e79a2ea3e606fd85b040f00316de64545c3ed4
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-09-17 16:33:04 -0400

    add back in eth test

--- server/ethereum/ethereum.test.js
+++ server/ethereum/ethereum.test.js
@@ -1,47 +1,46 @@
-// const Web3 = require('web3')
-// const { registerWatcher } = require('./')
-// const ipfsWrapper = require('../ipfs')
-// const { demoSmartContractJson } = require('../../mockData')
-// const accounts = require('../../accounts.json')
+const Web3 = require('web3')
+const { registerWatcher } = require('./')
+const ipfsWrapper = require('../ipfs')
+const { demoSmartContractJson1 } = require('../../mockData')
+const accounts = require('../../accounts.json')
 
-// let contract
-
-// const web3 = new Web3(
-//   new Web3.providers.WebsocketProvider('ws://localhost:8545')
-// )
-
-// let node
+let web3
+let contract
+let node
 
 beforeAll(() => {
-  // contract = new web3.eth.Contract(
-  //   demoSmartContractJson.abi,
-  //   demoSmartContractJson.address
-  // )
-  // const ipfs = ipfsWrapper({
-  //   host: process.env.IPFS_NODE_HOST ? process.env.IPFS_NODE_HOST : 'localhost',
-  //   port: process.env.IPFS_NODE_PORT ? process.env.IPFS_NODE_PORT : '5001',
-  //   protocol: process.env.IPFS_NODE_PROTOCOL
-  //     ? process.env.IPFS_NODE_PROTOCOL
-  //     : 'http',
-  //   headers: null
-  // })
-  // node = ipfs.node
+  web3 = new Web3(new Web3.providers.WebsocketProvider('ws://localhost:8545'))
+  contract = new web3.eth.Contract(
+    demoSmartContractJson1.abi,
+    demoSmartContractJson1.address
+  )
+  const ipfs = ipfsWrapper({
+    host: process.env.IPFS_NODE_HOST ? process.env.IPFS_NODE_HOST : 'localhost',
+    port: process.env.IPFS_NODE_PORT ? process.env.IPFS_NODE_PORT : '5001',
+    protocol: process.env.IPFS_NODE_PROTOCOL
+      ? process.env.IPFS_NODE_PROTOCOL
+      : 'http',
+    headers: null
+  })
+  node = ipfs.node
+})
+
+afterAll(() => {
+  web3.currentProvider.connection.close()
 })
 
 // NOTE - this isn't really testing anything yet, just showing how to fire an event and getting the testing framework working
 test('firing an event pins a file', async done => {
-  expect(true).toBe(true)
-  // const testKey = web3.utils.fromAscii('testKey')
-  // const testVal = { dag: 'killinnnnnn it' }
-  // const hash = await node.dag.put(testVal)
-  done()
+  const testKey = web3.utils.fromAscii('testKey')
+  const testVal = { dag: 'killinnnnnn it' }
+  const hash = await node.dag.put(testVal)
 
-  // registerWatcher(contract)
-  // contract.methods
-  //   .registerData(testKey, hash.toBaseEncodedString())
-  //   .send({ from: accounts[0] }, (err, res) => {
-  //     setTimeout(() => {
-  //       done()
-  //     }, 2000)
-  //   })
+  registerWatcher(contract)
+  contract.methods
+    .registerData(testKey, hash.toBaseEncodedString())
+    .send({ from: accounts[0] }, () => {
+      setTimeout(() => {
+        done()
+      }, 2000)
+    })
 })
