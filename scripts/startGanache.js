9be4d42f39c6657e0885debe28c618dedfff8695
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-09-18 16:43:01 -0400

    fix conditionals

--- scripts/startGanache.js
+++ scripts/startGanache.js
@@ -4,22 +4,23 @@ const isPortTaken = require('./isPortTaken')
 
 const startGanache = () =>
   new Promise(async (resolve, reject) => {
-    const shouldStartNewServer = await isPortTaken('8545')
-    if (shouldStartNewServer) {
+    const blockchainRunning = await isPortTaken('8545')
+    if (!blockchainRunning) {
       const server = ganache.server({ network_id: 123, block_time: 3 })
       server.listen(8545, (err, blockchain) => {
-        if (err) reject(err)
+        if (err) return reject(err)
         fs.writeFile(
           './accounts.json',
           JSON.stringify(Object.keys(blockchain.accounts)),
           error => {
             if (err) reject(error)
-            resolve(true)
+            return resolve(true)
           }
         )
       })
+    } else {
+      resolve(false)
     }
-    resolve(false)
   })
 
 module.exports = startGanache
