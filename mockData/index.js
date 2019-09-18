2a5369794d5645e507b85dcb5d1e007c64c8462b
Zach Sherman <zach@shrmn.toys>
2019-09-18 18:44:43 -0400

    remove network key from demo data

--- mockData/index.js
+++ mockData/index.js
@@ -2,13 +2,11 @@ const { abi, networks } = require('../build/contracts/Storage.json')
 
 const demoSmartContractJson1 = {
   address: networks['123'].address,
-  network: 'mainnet',
   abi
 }
 
 const demoSmartContractJson2 = {
   address: networks['123'].address.replace('0', '1'),
-  network: 'mainnet',
   abi
 }
 
