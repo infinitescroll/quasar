fbbf7e51f43e90d2021ce26dee9d908092e8b1d5
Zach Sherman <zach@shrmn.toys>
2019-09-24 13:31:56 -0400

    even better mock data

--- mockData/index.js
+++ mockData/index.js
@@ -5,7 +5,7 @@ const demoSmartContractJson1 = {
   abi
 }
 
-const address2 = networks['123'].address.slice(0, -1) + 'z'
+const address2 = '0x99E331Fa7c45671643666715309a176Ea4EEf919'
 const demoSmartContractJson2 = {
   address: address2,
   abi
