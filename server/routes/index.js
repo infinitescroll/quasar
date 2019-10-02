c1cb1a63eb2a05937cfa505070c18f40313da57e
listenaddress <tbobrien612@gmail.com>
2019-10-02 12:51:24 -0400

    Add mongo entries to routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,14 +1,29 @@
 const router = require('express').Router()
 const ipfs = require('../ipfs')
+const { Pin } = require('../db')
 const multer = require('multer')
 const upload = multer()
+var sizeof = require('object-sizeof')
 module.exports = router
 
 router.post('/dag/put', async (req, res, _next) => {
   try {
-    const result = await ipfs.node.dag.put(req.body)
-    // Save mongo entry
-    res.status(200).send(result.toBaseEncodedString())
+    const result = await ipfs.node.dag.put(req.body.dag)
+    const size = sizeof(req.body.dag)
+    const pin = new Pin({
+      size: size,
+      cid: result.toBaseEncodedString(),
+      smartContract: req.body.smartContract,
+      time: new Date()
+    })
+
+    pin.save(error => {
+      if (error) {
+        res.status(400).send(error)
+      } else {
+        res.status(201).send(result.toBaseEncodedString())
+      }
+    })
   } catch (error) {
     console.error(error)
     res.status(400).send(error)
@@ -18,8 +33,20 @@ router.post('/dag/put', async (req, res, _next) => {
 router.post('/file/add', upload.single('entry'), async (req, res, _next) => {
   try {
     const result = await ipfs.node.add(req.file.buffer)
-    // Save mongo entry
-    res.status(200).send(result[0].hash)
+    const pin = new Pin({
+      size: req.file.size,
+      cid: result[0].hash,
+      smartContract: req.body.smartContract,
+      time: new Date()
+    })
+
+    pin.save(error => {
+      if (error) {
+        res.status(400).send(error)
+      } else {
+        res.status(200).send(result[0].hash)
+      }
+    })
   } catch (error) {
     console.error(error)
     res.status(400).send(error)
