6d88f5d81419a77d93af72060b9fc1b9998148e8
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-10-02 14:28:15 -0400

    commenting to get more insight

--- server/ethereum/ethereum.test.js
+++ server/ethereum/ethereum.test.js
@@ -101,127 +101,127 @@ from pinning contract (without registering pinner) pins file`, async done => {
         done()
       }, 2500)
     })
-})
+}, 7500)
 
-test('watcher pins file from registerData function', async done => {
-  const testKey = web3.utils.fromAscii('testKey')
-  const dag = { testKey: 'testVal' }
-  const hash = await node.dag.put(dag)
-  await removeHashIfPinned(hash.toBaseEncodedString())
+// test('watcher pins file from registerData function', async done => {
+//   const testKey = web3.utils.fromAscii('testKey')
+//   const dag = { testKey: 'testVal' }
+//   const hash = await node.dag.put(dag)
+//   await removeHashIfPinned(hash.toBaseEncodedString())
 
-  await listenerUnsubscribe()
-  registerPinWatcher(contract)
+//   await listenerUnsubscribe()
+//   registerPinWatcher(contract)
 
-  contract.methods
-    .registerData(testKey, hash.toBaseEncodedString())
-    .send({ from: accounts[0] }, () => {
-      setTimeout(async () => {
-        const pins = await node.pin.ls()
-        const match = pins.find(
-          item => item.hash === hash.toBaseEncodedString()
-        )
-        expect(match).toBeDefined()
-        done()
-      }, 1000)
-    })
-})
+//   contract.methods
+//     .registerData(testKey, hash.toBaseEncodedString())
+//     .send({ from: accounts[0] }, () => {
+//       setTimeout(async () => {
+//         const pins = await node.pin.ls()
+//         const match = pins.find(
+//           item => item.hash === hash.toBaseEncodedString()
+//         )
+//         expect(match).toBeDefined()
+//         done()
+//       }, 1000)
+//     })
+// })
 
-test('firing a listen event adds a new contract to state + unsubscribing removes one', async done => {
-  const registerContract = contract =>
-    new Promise(resolve => {
-      listenerContract.methods
-        .listenToContract(contract.address)
-        .send({ from: accounts[0] }, () => {
-          setTimeout(() => {
-            resolve()
-          }, 500)
-        })
-    })
+// test('firing a listen event adds a new contract to state + unsubscribing removes one', async done => {
+//   const registerContract = contract =>
+//     new Promise(resolve => {
+//       listenerContract.methods
+//         .listenToContract(contract.address)
+//         .send({ from: accounts[0] }, () => {
+//           setTimeout(() => {
+//             resolve()
+//           }, 500)
+//         })
+//     })
 
-  await Promise.all([
-    await registerContract(demoSmartContractJson1),
-    await registerContract(demoSmartContractJson2)
-  ])
+//   await Promise.all([
+//     await registerContract(demoSmartContractJson1),
+//     await registerContract(demoSmartContractJson2)
+//   ])
 
-  expect(smartContracts.get()[0].address).toBe(demoSmartContractJson1.address)
-  expect(smartContracts.get()[0]).toHaveProperty('listener')
+//   expect(smartContracts.get()[0].address).toBe(demoSmartContractJson1.address)
+//   expect(smartContracts.get()[0]).toHaveProperty('listener')
 
-  expect(smartContracts.get()[1].address).toBe(demoSmartContractJson2.address)
-  expect(smartContracts.get()[1]).toHaveProperty('listener')
+//   expect(smartContracts.get()[1].address).toBe(demoSmartContractJson2.address)
+//   expect(smartContracts.get()[1]).toHaveProperty('listener')
 
-  listenerContract.methods
-    .unsubscribeContract(demoSmartContractJson1.address)
-    .send({ from: accounts[0] }, () => {
-      setTimeout(() => {
-        expect(smartContracts.get().length).toBe(1)
-        expect(smartContracts.get()[0].address).toBe(
-          demoSmartContractJson2.address
-        )
-        expect(smartContracts.get()[0]).toHaveProperty('listener')
-        done()
-      }, 1000)
-    })
-})
+//   listenerContract.methods
+//     .unsubscribeContract(demoSmartContractJson1.address)
+//     .send({ from: accounts[0] }, () => {
+//       setTimeout(() => {
+//         expect(smartContracts.get().length).toBe(1)
+//         expect(smartContracts.get()[0].address).toBe(
+//           demoSmartContractJson2.address
+//         )
+//         expect(smartContracts.get()[0]).toHaveProperty('listener')
+//         done()
+//       }, 1000)
+//     })
+// })
 
-test('handleListenEvent adds smart contract to state', async done => {
-  const eventObj = {
-    returnValues: { contractAddress: demoSmartContractJson1.address }
-  }
+// test('handleListenEvent adds smart contract to state', async done => {
+//   const eventObj = {
+//     returnValues: { contractAddress: demoSmartContractJson1.address }
+//   }
 
-  await handleListenEvent(null, eventObj)
-  expect(smartContracts.get()[0].address).toBe(demoSmartContractJson1.address)
-  expect(smartContracts.get()[0]).toHaveProperty('listener')
-  done()
-})
+//   await handleListenEvent(null, eventObj)
+//   expect(smartContracts.get()[0].address).toBe(demoSmartContractJson1.address)
+//   expect(smartContracts.get()[0]).toHaveProperty('listener')
+//   done()
+// })
 
-test('handleListenEvent throws error with empty params', async done => {
-  await expect(handleListenEvent()).rejects.toThrow()
-  done()
-})
+// test('handleListenEvent throws error with empty params', async done => {
+//   await expect(handleListenEvent()).rejects.toThrow()
+//   done()
+// })
 
-test('handlePinHashEvent pins file of cid it was passed', async done => {
-  const dag = { secondTestKey: 'secondTestVal' }
-  const cid = await node.dag.put(dag)
+// test('handlePinHashEvent pins file of cid it was passed', async done => {
+//   const dag = { secondTestKey: 'secondTestVal' }
+//   const cid = await node.dag.put(dag)
 
-  const eventObj = {
-    returnValues: {
-      cid: cid.toBaseEncodedString()
-    }
-  }
+//   const eventObj = {
+//     returnValues: {
+//       cid: cid.toBaseEncodedString()
+//     }
+//   }
 
-  const res = await handlePinHashEvent(null, eventObj)
+//   const res = await handlePinHashEvent(null, eventObj)
 
-  expect(res[0].hash).toBe(cid.toBaseEncodedString())
-  done()
-})
+//   expect(res[0].hash).toBe(cid.toBaseEncodedString())
+//   done()
+// })
 
-test('handlePinHashEvent throws error with empty params', async done => {
-  await expect(handlePinHashEvent()).rejects.toThrow()
-  done()
-})
+// test('handlePinHashEvent throws error with empty params', async done => {
+//   await expect(handlePinHashEvent()).rejects.toThrow()
+//   done()
+// })
 
-test('getContract returns a contract', async done => {
-  const contract = getContract(
-    demoSmartContractJson1,
-    demoSmartContractJson1.address
-  )
-  expect(contract._address).toBe(demoSmartContractJson1.address)
-  done()
-})
+// test('getContract returns a contract', async done => {
+//   const contract = getContract(
+//     demoSmartContractJson1,
+//     demoSmartContractJson1.address
+//   )
+//   expect(contract._address).toBe(demoSmartContractJson1.address)
+//   done()
+// })
 
-// this test must go last bc it mutates demoSmartContractJson1!!
-test('getContract throws when an invalid contract is passed', async done => {
-  demoSmartContractJson1.address = '0x7505462c30102eBCDA555446c3807362AeFEfc8r'
-  const badCall = () => {
-    return getContract(
-      demoSmartContractJson1,
-      '0x7505462c30102eBCDA555446c3807362AeFEfc8r'
-    )
-  }
+// // this test must go last bc it mutates demoSmartContractJson1!!
+// test('getContract throws when an invalid contract is passed', async done => {
+//   demoSmartContractJson1.address = '0x7505462c30102eBCDA555446c3807362AeFEfc8r'
+//   const badCall = () => {
+//     return getContract(
+//       demoSmartContractJson1,
+//       '0x7505462c30102eBCDA555446c3807362AeFEfc8r'
+//     )
+//   }
 
-  expect(badCall).toThrow()
-  done()
-})
+//   expect(badCall).toThrow()
+//   done()
+// })
 
 // Uncomment this when timeout in ipfs.dag.get is working. Will need to adjust jest.setTimeout.Timeout past the dag.get timeout time.
 
