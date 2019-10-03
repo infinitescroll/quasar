2709f6df888d1e41a4ecb68857af1a24b08f4716
Jon <Schwartz10@users.noreply.github.com>
2019-10-03 10:33:46 -0400

    Move contracts from memory to mongo (#38)
    * add pinEventSource schema and fix tests
    
    * fix tests, add and remove cnts from db

---
+++ server/db/index.js
@@ -0,0 +1,4 @@
+const Pin = require('./pinSchema')
+const SmartContractToPoll = require('./smartContractToPollSchema')
+
+module.exports = { Pin, SmartContractToPoll }
--- server/db/pinSchema.test.js
+++ server/db/pinSchema.test.js
@@ -1,82 +1,66 @@
 const mongoose = require('mongoose')
 const Pin = require('./pinSchema')
 const ipfs = require('../ipfs')
-const { demoSmartContractJson1 } = require('../../mockData')
 const ttl = process.env.TTL || 14
-let oldDate = new Date()
+const oldDate = new Date()
 oldDate.setDate(oldDate.getDate() - ttl - 1)
-
-const demoNewPin1 = new Pin({
-  size: 2000,
-  cid: 'abc',
-  smartContract: demoSmartContractJson1.address,
-  time: new Date()
-})
-
-const demoNewPin2 = new Pin({
-  size: 2000,
-  cid: 'def',
-  smartContract: demoSmartContractJson1.address,
-  time: new Date()
-})
-
-const demoOldPin1 = new Pin({
-  size: 2000,
-  cid: 'abc',
-  smartContract: demoSmartContractJson1.address,
-  time: oldDate
-})
-
-const demoOldPin2 = new Pin({
-  size: 2000,
-  cid: 'def',
-  smartContract: demoSmartContractJson1.address,
-  time: oldDate
-})
 
 let cid1, cid2
 beforeAll(async done => {
   await mongoose.connect(process.env.DB_URL || 'mongodb://localhost/test', {
     useNewUrlParser: true
   })
-  await Pin.deleteMany({}).exec()
+  mongoose.connection.db.dropDatabase()
 
   cid1 = await ipfs.node.dag.put({ test: 'data1' })
   cid1 = cid1.toBaseEncodedString()
+  await ipfs.node.pin.add(cid1)
   cid2 = await ipfs.node.dag.put({ test: 'data2' })
   cid2 = cid2.toBaseEncodedString()
+  await ipfs.node.pin.add(cid2)
 
-  demoNewPin1.cid = cid1
-  demoOldPin1.cid = cid1
-  demoOldPin2.cid = cid2
-  demoNewPin2.cid = cid2
+  await Pin.create({
+    size: 2000,
+    cid: cid1,
+    time: new Date()
+  })
+
+  await Pin.create({
+    size: 2000,
+    cid: cid2,
+    time: new Date()
+  })
+
+  await Pin.create({
+    size: 2000,
+    cid: cid1,
+    time: oldDate
+  })
+
+  await Pin.create({
+    size: 2000,
+    cid: cid2,
+    time: oldDate
+  })
+
   done()
 })
+
 test('can add and retrieve well-formed pins from db', async done => {
-  await demoNewPin1.save()
   const pins1 = await Pin.find({ cid: cid1 }).exec()
   expect(pins1.length > 0).toBe(true)
 
-  await demoNewPin2.save()
   const pins2 = await Pin.find({ cid: cid2 }).exec()
   expect(pins2.length > 0).toBe(true)
   done()
 })
 
 test('can add + remove old pins from db', async done => {
-  let cuttoff = new Date()
+  const cuttoff = new Date()
   cuttoff.setDate(cuttoff.getDate() - ttl)
 
-  await demoOldPin1.save()
-  const pins1 = await Pin.find({ cid: cid1, time: { $lt: cuttoff } }).exec()
-  expect(pins1.length > 0).toBe(true)
-
-  await demoOldPin2.save()
-  const pins2 = await Pin.find({ cid: cid2, time: { $lt: cuttoff } }).exec()
-  expect(pins2.length > 0).toBe(true)
-
-  await ipfs.node.pin.add(cid1)
-  await ipfs.node.pin.add(cid2)
+  const outdatedPins = await Pin.find({ time: { $lt: cuttoff } }).exec()
+  expect(outdatedPins.length).toBe(2)
 
   await Pin.findandRemoveOldPins()
   const oldPins = await Pin.find({ time: { $lt: cuttoff } })
@@ -89,7 +73,6 @@ test('adding malformed pin object throws', async done => {
   const badCid = new Pin({
     cid: 89,
     time: new Date(),
-    smartContract: demoSmartContractJson1.address,
     size: 2000
   })
 
@@ -101,14 +84,6 @@ test('adding malformed pin object throws', async done => {
     cid: 'fake cid'
   })
 
-  const badSmartContract = new Pin({
-    smartContract: 'fake smartContract',
-    cid: cid1,
-    time: new Date(),
-    size: 2000
-  })
-
-  await expect(badSmartContract.save()).rejects.toThrow()
   await expect(badCid.save()).rejects.toThrow()
   await expect(badCid2.save()).rejects.toThrow()
   await expect(missingFields.save()).rejects.toThrow()
@@ -117,7 +92,6 @@ test('adding malformed pin object throws', async done => {
       bad: 'field',
       cid: cid1,
       time: new Date(),
-      smartContract: demoSmartContractJson1.address,
       size: 2000
     })
   }).toThrow()
