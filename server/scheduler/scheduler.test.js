51035a4e3702728b927e0f88c6f5a6760597f80d
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-10-16 04:30:20 -0400

    describe tests

--- server/scheduler/scheduler.test.js
+++ server/scheduler/scheduler.test.js
@@ -1,23 +1,25 @@
 const Scheduler = require('./')
 
-test('calls callback once at the beginning and once at every interval', done => {
-  const callback = jest.fn()
-  const taskRunner = new Scheduler(callback, 100)
-  setTimeout(() => {
-    expect(callback.mock.calls.length).toBe(3)
-    taskRunner.stop()
-    done()
-  }, 299)
-})
-
-test('does not callback after stop command is issued', done => {
-  const callback = jest.fn()
-  const taskRunner = new Scheduler(callback, 100)
-  setTimeout(() => {
-    taskRunner.stop()
+describe('job scheduler', () => {
+  test('calls callback once at the beginning and once at every interval', done => {
+    const callback = jest.fn()
+    const taskRunner = new Scheduler(callback, 100)
     setTimeout(() => {
-      expect(callback.mock.calls.length).toBe(1)
+      expect(callback.mock.calls.length).toBe(3)
+      taskRunner.stop()
       done()
     }, 299)
-  }, 99)
+  })
+
+  test('does not callback after stop command is issued', done => {
+    const callback = jest.fn()
+    const taskRunner = new Scheduler(callback, 100)
+    setTimeout(() => {
+      taskRunner.stop()
+      setTimeout(() => {
+        expect(callback.mock.calls.length).toBe(1)
+        done()
+      }, 299)
+    }, 99)
+  })
 })
