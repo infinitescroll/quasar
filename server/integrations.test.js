49fca34df0137130af8c72d966a1ef64899da317
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2019-10-17 00:43:56 -0400

    one more attempt

--- server/integrations.test.js
+++ server/integrations.test.js
@@ -88,62 +88,66 @@ afterAll(() => {
 describe('integration tests', () => {
   describe('polling mechanisms', () => {
     test('firing listen event adds contract to db and begins polling, unsubscribing removes contract from db', async done => {
-      const server = app.listen('9091')
-      await Promise.all([
-        await emitListenToContractEvent(demoSmartContractJson1.address),
-        await emitListenToContractEvent(demoSmartContractJson2.address)
-      ])
-      const smartContractToPoll = await SmartContractToPoll.findOne({
-        address: demoSmartContractJson1.address
-      })
-      expect(smartContractToPoll.address).toBe(demoSmartContractJson1.address)
-      expect(smartContractToPoll.sizeOfPinnedData).toBe(0)
-      expect(smartContractToPoll.lastPolledBlock).toBeGreaterThan(0)
-      const secondSmartContractToPoll = await SmartContractToPoll.findOne({
-        address: demoSmartContractJson2.address
-      })
-      expect(secondSmartContractToPoll.address).toBe(
-        demoSmartContractJson2.address
-      )
-      expect(secondSmartContractToPoll.sizeOfPinnedData).toBe(0)
-      expect(secondSmartContractToPoll.lastPolledBlock).toBeGreaterThan(0)
-      await listenerUnsubscribe(demoSmartContractJson1.address)
+      const server = app.listen('9091', async () => {
+        await Promise.all([
+          await emitListenToContractEvent(demoSmartContractJson1.address),
+          await emitListenToContractEvent(demoSmartContractJson2.address)
+        ])
+        const smartContractToPoll = await SmartContractToPoll.findOne({
+          address: demoSmartContractJson1.address
+        })
+        expect(smartContractToPoll.address).toBe(demoSmartContractJson1.address)
+        expect(smartContractToPoll.sizeOfPinnedData).toBe(0)
+        expect(smartContractToPoll.lastPolledBlock).toBeGreaterThan(0)
+        const secondSmartContractToPoll = await SmartContractToPoll.findOne({
+          address: demoSmartContractJson2.address
+        })
+        expect(secondSmartContractToPoll.address).toBe(
+          demoSmartContractJson2.address
+        )
+        expect(secondSmartContractToPoll.sizeOfPinnedData).toBe(0)
+        expect(secondSmartContractToPoll.lastPolledBlock).toBeGreaterThan(0)
+        await listenerUnsubscribe(demoSmartContractJson1.address)
 
-      const removedSmartContractToPoll = await SmartContractToPoll.findOne({
-        address: demoSmartContractJson1.address
+        const removedSmartContractToPoll = await SmartContractToPoll.findOne({
+          address: demoSmartContractJson1.address
+        })
+        const nonRemovedSmartContractToPoll = await SmartContractToPoll.findOne(
+          {
+            address: demoSmartContractJson2.address
+          }
+        )
+        expect(removedSmartContractToPoll).toBe(null)
+        expect(nonRemovedSmartContractToPoll.address).toBe(
+          demoSmartContractJson2.address
+        )
+        server.close(done)
       })
-      const nonRemovedSmartContractToPoll = await SmartContractToPoll.findOne({
-        address: demoSmartContractJson2.address
-      })
-      expect(removedSmartContractToPoll).toBe(null)
-      expect(nonRemovedSmartContractToPoll.address).toBe(
-        demoSmartContractJson2.address
-      )
-      server.close(done)
     })
   })
 
   test(`emitting listen event to listener contractt, then emittting pinHash event to storage contract, removes associated document from database`, async done => {
-    const server = app.listen('9091')
-    // set up smart contract
-    await emitListenToContractEvent(demoSmartContractJson1.address)
+    const server = app.listen('9091', async () => {
+      // set up smart contract
+      await emitListenToContractEvent(demoSmartContractJson1.address)
 
-    const testKey = web3.utils.fromAscii('testKey')
-    const dag = { testKey: 'testVal' }
-    const hash = await node.dag.put(dag)
+      const testKey = web3.utils.fromAscii('testKey')
+      const dag = { testKey: 'testVal' }
+      const hash = await node.dag.put(dag)
 
-    // manually add pin to db, to be removed later
-    await Pin.create({
-      size: 100,
-      cid: hash.toBaseEncodedString(),
-      time: new Date()
+      // manually add pin to db, to be removed later
+      await Pin.create({
+        size: 100,
+        cid: hash.toBaseEncodedString(),
+        time: new Date()
+      })
+      await emitPinHashEvent(testKey, hash.toBaseEncodedString())
+      const removedPinFile = await Pin.findOne({
+        cid: hash.toBaseEncodedString()
+      })
+
+      expect(removedPinFile).toBe(null)
+      server.close(done)
     })
-    await emitPinHashEvent(testKey, hash.toBaseEncodedString())
-    const removedPinFile = await Pin.findOne({
-      cid: hash.toBaseEncodedString()
-    })
-
-    expect(removedPinFile).toBe(null)
-    server.close(done)
   })
 })
